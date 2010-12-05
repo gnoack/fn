d0dfdd1e93524b8502b3bb3155bd2a5a8b16c8d3
Günther Noack <guenther@unix-ag.uni-kl.de>
2010-12-05 17:39:45 +0100

    string interning

--- primitives/string-interning-test.c
+++ primitives/string-interning-test.c
@@ -3,16 +3,54 @@
 #include "tests.h"
 #include "string-interning.h"
 
-TEST(simple_interning) {
+// template must be a string of length 3 or more.
+const char* make(const char* template) {
   char* foo = malloc(4);
-  foo[0] = 'f';
-  foo[1] = 'o';
-  foo[2] = 'o';
+  foo[0] = template[0];
+  foo[1] = template[1];
+  foo[2] = template[2];
   foo[3] = '\0';
+  return foo;
+}
+
+TEST(simple_interning) {
+  // Returns a string with the same content.
+  const char* foo = make("foo");
   const char* result = intern_string(foo);
   ASSERT_TRUE(strcmp("foo", result) == 0);
 }
 
+TEST(interns_to_different_values) {
+  const char* bar = make("bar");
+  const char* baz = make("baz");
+  ASSERT_TRUE(intern_string(bar) != intern_string(baz));
+}
+
+TEST(interning_different_values) {
+  const char* foo = "foo";
+  const char* result = intern_string(foo);
+  ASSERT_TRUE(foo != result);
+}
+
+TEST(interning_idempotent) {
+  const char* foo1 = make("foo");
+  const char* foo2 = intern_string(foo1);
+  const char* foo3 = intern_string(foo2);
+  ASSERT_TRUE(foo1 != foo2);
+  ASSERT_TRUE(foo2 == foo3);
+}
+
+TEST(interning_interns) {
+  const char* foo1 = make("foo");
+  const char* foo2 = make("foo");
+  ASSERT_TRUE(foo1 != foo2);
+  ASSERT_TRUE(intern_string(foo1) == intern_string(foo2));
+}
+
 void interning_tests() {
   TESTRUN(simple_interning);
+  TESTRUN(interns_to_different_values);
+  TESTRUN(interning_different_values);
+  TESTRUN(interning_idempotent);
+  TESTRUN(interning_interns);
 }
--- primitives/string-interning.c
+++ primitives/string-interning.c
@@ -1,9 +1,47 @@
 
+#include <malloc.h>
 #include <err.h>
+#include <string.h>
+
+#include "value.h"
 
 /* This is an intentionally simple implementation of string
    interning. */
 
+// 4 kb of interned strings should be enough for now.
+char* interned_strings = NULL;
+
+#define INTERNED_DB_SIZE (4 * 1024)
+
+bool matches(const char* a, const char* b) {
+  return TO_BOOL(strcmp(a, b) == 0);
+}
+
+bool is_last(const char* s) {
+  return TO_BOOL(s[0] == '\0');
+}
+
 const char* intern_string(const char* s) {
-  return s;
+  if (interned_strings == NULL) {
+    interned_strings = malloc(INTERNED_DB_SIZE);
+    interned_strings[0] = '\0';
+  }
+
+  char* here_be_dragons = interned_strings + INTERNED_DB_SIZE;
+  char* curr = interned_strings;
+
+  while(!is_last(curr)) {
+    if (matches(curr, s)) {
+      return curr;
+    } else {
+      curr = curr + strlen(curr) + 1;
+    }
+  }
+  // Append.
+  if (curr + strlen(s) + 1 >= here_be_dragons) {
+    errx(-1, "String %s can't be interned (doesn't fit).\n", s);
+  }
+  strcpy(curr, s);
+  curr[strlen(s) + 1] = '\0';
+  return curr;
 }
