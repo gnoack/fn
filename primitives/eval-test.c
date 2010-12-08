cb5704a0d557b9b86ec18f0fc406ed2bb1ea3d1d
Günther Noack <guenther@unix-ag.uni-kl.de>
2010-12-08 22:12:45 +0100

    test with cascaded if statements

--- primitives/eval-test.c
+++ primitives/eval-test.c
@@ -14,6 +14,13 @@ TEST(eval_if_false) {
   ASSERT_EQ(I(50), eval(LIST(S("if"), NIL, I(5), I(50))));
 }
 
+TEST(eval_if_cascaded) {
+  ASSERT_EQ(I(5), eval(LIST(S("if"),
+			    LIST(S("if"), NIL, NIL, I(4)),
+			    I(5),
+			    I(50))));
+}
+
 // TODO(gnoack): Tests for: Lambda, Cond.
 // TODO(gnoack): Differ between true, false, nil and 0.
 
@@ -21,4 +28,5 @@ extern
 void eval_tests() {
   TESTRUN(eval_if_true);
   TESTRUN(eval_if_false);
+  TESTRUN(eval_if_cascaded);
 }
