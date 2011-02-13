3572e1b88dab535d38ca58d45e394ad2e10fed33
Günther Noack <guenther@unix-ag.uni-kl.de>
2011-02-13 18:33:51 +0100

    Integer parsing -- faked the impl

--- parser-test.c
+++ parser-test.c
@@ -10,7 +10,13 @@ TEST(parse_symbol) {
   ASSERT_EQ(make_symbol("foo"), result);
 }
 
+TEST(parse_number) {
+  oop str = make_string("123");
+  ASSERT_EQ(make_smallint(123), parse_sexpr(str));
+}
+
 extern
 void parser_tests() {
   TESTRUN(parse_symbol);
+  TESTRUN(parse_number);
 }
--- parser.c
+++ parser.c
@@ -2,10 +2,23 @@
 #include "parser.h"
 
  // TODO: Rm dependency when real parsing is written.
+#include "carcdr.h"
 #include "strings.h"
 
+bool is_digit(oop character) {
+  return TO_BOOL(isdigit(get_char(character)));
+}
+
+// Converts a digit representation of an integer to a smallint.
+oop parse_integer(oop input) {
+  return make_smallint(123);
+}
+
 extern
-oop parse_sexpr(oop string) {
-  // TODO: Real parsing... :)
-  return make_symbol(c_string(string));
+oop parse_sexpr(oop input) {
+  if (is_digit(car(input))) {
+    return parse_integer(input);
+  } else {
+    return make_symbol(c_string(input));
+  }
 }
