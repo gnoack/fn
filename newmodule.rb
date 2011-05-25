df08827c85cdd35ed56160464ad7621669c66487
Günther Noack <guenther@unix-ag.uni-kl.de>
2011-05-25 19:15:03 +0200

    Generate empty lisp modules

--- newmodule.rb
+++ newmodule.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/ruby
 
 if ARGV.empty?
-  puts 'Usage: newmodule.rb MODULENAME'
+  puts 'Usage: newmodule.rb [--lisp] MODULENAME'
   exit(0)
 end
 
 modname = ARGV[0]
+lisp_mode = false
+
+if modname == '--lisp'
+  modname = ARGV[1]
+  lisp_mode = true
+end
 
 files = []
 
@@ -22,26 +28,41 @@ def guard(file, guarddef)
     "#endif  // #{guarddef}\n"
 end
 
-files << ["#{modname}.c", newline + mkinclude(modname)]
-files << ["#{modname}.h", guard(newline + mkinclude('value'),
-                                "_#{modname.upcase}_H_")]
 files << ["#{modname}-test.h",
           guard(newline + "extern" +
                 newline + "void #{modname}_tests();" +
                 newline,
                 "_#{modname.upcase}_TEST_H_")]
-files << ["#{modname}-test.c",
-          newline + mkinclude("tests") +
-          newline + mkinclude(modname) +
-          newline + "TEST(#{modname}_simple) {" +
-          newline + "  // TODO" +
-          newline + "}" +
-          newline +
-          newline + "extern" +
-          newline + "void #{modname}_tests() {" +
-          newline + "  TESTRUN(#{modname}_simple);" +
-          newline + "}" +
-          newline]
+
+if lisp_mode
+  empty_lisp_file = newline + "(" +
+    newline + ")" +
+    newline
+
+  files << ["#{modname}.h",
+            guard(newline + "extern" +
+                  newline + "void #{modname}_decls();" +
+                  newline,
+                  "_#{modname.upcase}_H_")]
+  files << ["#{modname}.fn", empty_lisp_file]
+  files << ["#{modname}-test.fn", empty_lisp_file]
+else
+  files << ["#{modname}.h", guard(newline + mkinclude('value'),
+                                  "_#{modname.upcase}_H_")]
+  files << ["#{modname}.c", newline + mkinclude(modname)]
+  files << ["#{modname}-test.c",
+            newline + mkinclude("tests") +
+            newline + mkinclude(modname) +
+            newline + "TEST(#{modname}_simple) {" +
+            newline + "  // TODO" +
+            newline + "}" +
+            newline +
+            newline + "extern" +
+            newline + "void #{modname}_tests() {" +
+            newline + "  TESTRUN(#{modname}_simple);" +
+            newline + "}" +
+            newline]
+end
 
 files.each do |f|
   filename, contents = f
@@ -60,3 +81,8 @@ puts "Remaining steps:"
 puts "* Now, in Makefile, add #{modname}-test.o, #{modname}.o."
 puts "* Also include #{modname}-test.h from tests.c, call"
 puts "  #{modname}_tests() from there!"
+if lisp_mode
+  puts "* Call load_decls(#{modname}_decls()) in init() in tests.c"
+  puts "  and include #{modname}.h there, too."
+end
+
