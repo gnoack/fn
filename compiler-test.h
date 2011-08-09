667b49b5bd22c5ebfdc2b61edbd5d0264c8313b1
Günther Noack <guenther@unix-ag.uni-kl.de>
2011-08-09 23:06:38 +0200

    functions for working with expressions

---
+++ compiler-test.h
@@ -0,0 +1,7 @@
+#ifndef _COMPILER_TEST_H_
+
+extern
+void compiler_tests();
+
+#define _COMPILER_TEST_H_ 0
+#endif  // _COMPILER_TEST_H_
---
+++ compiler.h
@@ -0,0 +1,9 @@
+#ifndef _COMPILER_H_
+
+#include "value.h"
+
+extern
+oop compiler_decls();
+
+#define _COMPILER_H_ 0
+#endif  // _COMPILER_H_
