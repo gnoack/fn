b6f38a9aaca7d0fdf430c757db9bfd71807f6b10
Günther Noack <guenther@unix-ag.uni-kl.de>
2011-08-29 21:45:19 +0200

    Forgot some maps-related files

---
+++ maps-test.h
@@ -0,0 +1,7 @@
+#ifndef _MAPS_TEST_H_
+
+extern
+void maps_tests();
+
+#define _MAPS_TEST_H_ 0
+#endif  // _MAPS_TEST_H_
---
+++ maps.h
@@ -0,0 +1,9 @@
+#ifndef _MAPS_H_
+
+#include "value.h"
+
+extern
+oop maps_decls();
+
+#define _MAPS_H_ 0
+#endif  // _MAPS_H_
