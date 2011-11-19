34bd2cf6f189eddaa1ebbd5197b2ea59bb9c72f8
Günther Noack <guenther@unix-ag.uni-kl.de>
2011-11-19 22:28:56 +0100

    Test that allocated space is zeroed out.

--- memory-test.c
+++ memory-test.c
@@ -10,7 +10,13 @@ TEST(memory_alloc) {
   ASSERT_EQ(make_smallint(99), mem_get(obj, 0));
 }
 
+TEST(memory_alloc_zero_by_default) {
+  oop obj = mem_alloc(1);
+  ASSERT_NIL(mem_get(obj, 0));
+}
+
 extern
 void memory_tests() {
   TESTRUN(memory_alloc);
+  TESTRUN(memory_alloc_zero_by_default);
 }
