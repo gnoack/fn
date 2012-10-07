19f5f5b7910b8302c101efd9dcecd0853c0720d9
Günther Noack <guenther@unix-ag.uni-kl.de>
2012-10-07 21:31:58 +0200

    Reimplement dictionaries with open addressing.
    http://en.wikipedia.org/wiki/Open_addressing
    
    Dropped `dict-remove!' for now.

--- data.c
+++ data.c
@@ -15,17 +15,12 @@ fn_uint symbol_to_hash(oop symbol) {
   return (symbol.smallint >> 1);
 }
 
-oop primitive_symbol_to_hash(oop args) {
-  PARSE_ONE_ARG(sym);
-  return make_smallint(symbol_to_hash(sym));
-}
-
 
 
 oop make_array(fn_uint array_size) {
   fn_uint header_size = 2;
   oop result = mem_alloc(header_size + array_size);
-  mem_set(result, 0, symbols._dict);
+  mem_set(result, 0, NIL);  // TODO: @array type.
   mem_set(result, 1, make_smallint(array_size));  // Size
   fn_uint i;
   for (i = 0; i < array_size; i++) {
@@ -60,6 +55,14 @@ oop array_get(oop array, fn_uint index) {
  * The table size is saved in the table itself.
  */
 
+oop make_dict(fn_uint table_size) {
+  oop result = mem_alloc(3);
+  mem_set(result, 0, symbols._dict);
+  mem_set(result, 1, make_smallint(0L));
+  mem_set(result, 2, make_array(table_size * 2));
+  return result;
+}
+
 fn_uint dict_size(oop dict) {
   return get_smallint(mem_get(dict, 1));
 }
@@ -68,117 +71,57 @@ oop dict_table(oop dict) {
   return mem_get(dict, 2);
 }
 
-fn_uint dict_table_size(oop dict) {
-  return array_size(dict_table(dict));
-}
-
-oop primitive_make_dict(oop args) {
-  CHECK(is_nil(args), "make-dict takes no arguments.");
-  fn_uint table_size = 5;
-  oop result = mem_alloc(3);
-  mem_set(result, 0, NIL);  // TODO: Type marker.
-  mem_set(result, 1, make_smallint(0L));
-  mem_set(result, 2, make_array(table_size));
-  return result;
-}
-
-// In `oop's, not in items.
-#define INNER_TABLE_SIZE 14
-
-// Returns 1 if element was put, 0 if it was replced.
-int inner_table_put(oop inner_table, oop key, oop value) {
-  int i;
-  int first_good_index = -1;
-  for (i=0; i<INNER_TABLE_SIZE; i+=2) {
-    oop stored_key = array_get(inner_table, i);
-    if (value_eq(stored_key, key)) {
-      array_set(inner_table, i + 1, value);
-      return 0;
-    } else if (first_good_index == -1 && is_nil(stored_key)) {
-      first_good_index = i;
-    }
-  }
-  if (first_good_index == -1) {
-    printf("No space to put key in dictionary.");
-    exit(1);
-  }
-  array_set(inner_table, first_good_index, key);
-  array_set(inner_table, first_good_index + 1, value);
-  return 1;
-}
-
-void inner_table_remove(oop inner_table, oop key) {
-  int i;
-  for (i=0; i<INNER_TABLE_SIZE; i+=2) {
-    oop stored_key = array_get(inner_table, i);
-    if (value_eq(stored_key, key)) {
-      array_set(inner_table, i, NIL);
-      return;
-    }
-  }
-  CHECKV(1==0, key, "Key not found in dictionary.");
-}
-
-oop inner_table_get(oop inner_table, oop key) {
-  int i;
-  for (i=0; i<INNER_TABLE_SIZE; i+=2) {
-    oop stored_key = array_get(inner_table, i);
-    if (value_eq(stored_key, key)) {
-      return array_get(inner_table, i + 1);
-    }
-  }
-  CHECKV(1==0, key, "Key not found in dictionary.");
-}
-
-oop inner_table_key_value_pairs(oop inner_table, oop accumulator) {
-  int i;
-  for (i=0; i<INNER_TABLE_SIZE; i+=2) {
-    oop key = array_get(inner_table, i);
-    if (!is_nil(key)) {
-      oop value = array_get(inner_table, i + 1);
-      accumulator = make_cons(make_cons(key, value), accumulator);
-    }
-  }
-  return accumulator;
-}
-
-
-oop dict_table_get_inner_table(oop table, oop key, boolean create_if_needed) {
-  fn_uint idx = symbol_to_hash(key) % array_size(table);
-  oop result = array_get(table, idx);
-  if (is_nil(result)) {
-    CHECKV(create_if_needed, key, "Key not found in dictionary.");
-    result = make_array(INNER_TABLE_SIZE);
-    array_set(table, idx, result);
-  }
-  return result;
-}
+
+/**
+ * Inner hash table.
+ *
+ * Table is an array of size 2n where the first n elements are keys,
+ * the second n elements are the corresponding values.
+ */
 
 // Returns 1 if element was put, 0 if it was replced.
 int dict_table_put(oop table, oop key, oop value) {
-  oop inner_table = dict_table_get_inner_table(table, key, YES);
-  return inner_table_put(inner_table, key, value);
+  int size = (int) array_size(table) >> 1;
+  int i = symbol_to_hash(key) % size;
+  oop current_key;
+  do {
+    i = (i + 1) % size;
+    current_key = array_get(table, i);
+    if (value_eq(current_key, key)) {
+      array_set(table, size + i, value);
+      return 0;
+    }
+  } while (!is_nil(current_key));
+  array_set(table, i, key);
+  array_set(table, size + i, value);
+  return 1;
 }
 
 oop dict_table_get(oop table, oop key) {
-  oop inner_table = dict_table_get_inner_table(table, key, NO);
-  return inner_table_get(inner_table, key);
-}
-
-void dict_table_remove(oop table, oop key) {
-  oop inner_table = dict_table_get_inner_table(table, key, NO);
-  inner_table_remove(inner_table, key);
+  int size = (int) array_size(table) >> 1;
+  int i = symbol_to_hash(key) % size;
+  oop current_key;
+  do {
+    i = (i + 1) % size;
+    current_key = array_get(table, i);
+    if (value_eq(current_key, key)) {
+      return array_get(table, size + i);
+    }
+  } while (!is_nil(current_key));
+  CHECKV(1==0, key, "Key not found.")
 }
 
 oop dict_table_key_value_pairs(oop table) {
-  fn_uint table_size = array_size(table);
-  oop result = NIL;
+  int size = array_size(table) >> 1;
   int i;
-  for (i=0; i<table_size; i++) {
-    oop inner_table = array_get(table, i);
-    if (!is_nil(inner_table)) {
-      result = inner_table_key_value_pairs(inner_table, result);
+  oop result = NIL;
+  for (i=0; i<size; i++) {
+    oop key = array_get(table, i);
+    if (is_nil(key)) {
+      continue;
     }
+    oop value = array_get(table, size + i);
+    result = make_cons(make_cons(key, value), result);
   }
   return result;
 }
@@ -186,63 +129,61 @@ oop dict_table_key_value_pairs(oop table) {
 // Destructively resizes the dictionary to a new size.
 void dict_resize(oop dict, fn_uint new_table_size) {
   oop old_table = dict_table(dict);
-  oop new_table = make_array(new_table_size);
-  fn_uint old_table_size = array_size(old_table);
+  oop new_table = make_array(new_table_size * 2);
+  fn_uint old_table_size = array_size(old_table) >> 1;
+  printf("Resize from %d to %d\n", old_table_size, new_table_size);
   int i, j;
   for (i=0; i<old_table_size; i++) {
-    oop inner_table = array_get(old_table, i);
-    if (is_nil(inner_table)) {
+    oop key = array_get(old_table, i);
+    if (is_nil(key)) {
       continue;
     }
-    for (j=0; j<INNER_TABLE_SIZE; j+=2) {
-      oop key = array_get(inner_table, j);
-      if (!is_nil(key)) {
-        oop value = array_get(inner_table, j + 1);
-        dict_table_put(new_table, key, value);
-      }
-    }
+    oop value = array_get(old_table, old_table_size + i);
+    dict_table_put(new_table, key, value);
   }
   mem_set(dict, 2, new_table);
 }
 
 void dict_change_count(oop dict, int amount) {
   fn_uint old_count = dict_size(dict);
-  fn_uint table_size = dict_table_size(dict);
+  fn_uint table_size = array_size(dict_table(dict)) >> 1;
   fn_uint new_count = old_count + amount;
   // TODO: Tweak factor.
-  if (new_count > table_size * 4) {
+  if (new_count > table_size / 2) {
     dict_resize(dict, table_size * 2);
   }
   mem_set(dict, 1, make_smallint(new_count));
 }
 
 
+/* Operations on dictionaries. */
+
+// TODO: Implement remove operation.
+// See http://en.wikipedia.org/wiki/Open_addressing
 
 oop dict_get(oop dict, oop key) {
   return dict_table_get(dict_table(dict), key);
 }
 
-void dict_remove(oop dict, oop key) {
-  dict_change_count(dict, -1);
-  dict_table_remove(dict_table(dict), key);
-}
-
 oop dict_put(oop dict, oop key, oop value) {
+  // TODO: Only resize when a new entry had to be added.
   int added_amount = dict_table_put(dict_table(dict), key, value);
   dict_change_count(dict, added_amount);
 }
 
 
+/*
+ * Lisp interface.
+ */
+
+oop primitive_symbol_to_hash(oop args) {
+  PARSE_ONE_ARG(sym);
+  return make_smallint(symbol_to_hash(sym));
+}
 
 oop primitive_dict_get(oop args) {
   PARSE_TWO_ARGS(dict, key);
   return dict_get(dict, key);
-}
-
-oop primitive_dict_remove(oop args) {
-  PARSE_TWO_ARGS(dict, key);
-  dict_remove(dict, key);
-  return dict;
 }
 
 oop primitive_dict_put(oop args) {
@@ -256,13 +197,19 @@ oop primitive_dict_key_value_pairs(oop args) {
   return dict_table_key_value_pairs(dict_table(dict));
 }
 
+oop primitive_make_dict(oop args) {
+  CHECK(is_nil(args), "make-dict takes no arguments.");
+  return make_dict(5);
+}
+
+
+
 void init_data() {
   // Some simple utilities defined in utils.fn.
   register_globally_fn("symbol->hash", primitive_symbol_to_hash);
   register_globally_fn("make-dict", primitive_make_dict);
   register_globally_fn("dict-get", primitive_dict_get);
   register_globally_fn("dict-put!", primitive_dict_put);
-  register_globally_fn("dict-remove!", primitive_dict_remove);
   register_globally_fn("dict-key-value-pairs",
                        primitive_dict_key_value_pairs);
 }
