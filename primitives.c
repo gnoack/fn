3753cf89adba3b5d4da5d008dc63a3bed348475c
Günther Noack <guenther@unix-ag.uni-kl.de>
2013-04-03 22:49:15 +0200

    Check for libdl errors and fix memory corruption bug.

--- primitives.c
+++ primitives.c
@@ -243,6 +243,9 @@ oop primitive_dlopen(oop args) {
   char* c_name = c_string(name);
   void* handle = dlopen(c_name, RTLD_LOCAL | RTLD_NOW);
   free(c_name);
+
+  char* error = dlerror();
+  CHECK(error == NULL, error);
   return make_smallint((fn_uint) handle);
 }
 
@@ -252,23 +255,30 @@ oop primitive_dlsym(oop args) {
   void* c_handle = (void*) get_smallint(handle);
   void* (*c_sym)(void* arg, ...) = dlsym(c_handle, c_symbolname);
   free(c_symbolname);
+
+  char* error = dlerror();
+  CHECK(error == NULL, error);
   return make_smallint((fn_uint) c_sym);
 }
 
 oop primitive_call_dlsym(oop args) {
-  // TOOD: Varargs.
+  // TODO: Varargs.
   PARSE_TWO_ARGS(sym, arg);
   void* (*c_sym)(void* arg, ...) = (void* (*)(void*, ...)) get_smallint(sym);
   char* c_arg = c_string(arg);
-  void* c_result = c_sym(c_arg);
+  void* c_result = c_sym(c_arg);  // Call.
   free(c_arg);
   oop result;
   if (c_result) {
     result = make_string((const char*) c_result);
+    // TODO: Memory leak: Some C functions like getenv() return
+    // non-freeable c strings, we should distinguish between the two cases.
+    if (((void*) c_result) < sbrk(0)) {  // On heap.  (Dirty hack :-/)
+      free(c_result);
+    }
   } else {
     result = NIL;
   }
-  free(c_result);
   return result;
 }
 
