3fe1e0284b3e1fd7dbda441d53346bcb262a6262
Günther Noack <guenther@unix-ag.uni-kl.de>
2014-12-10 23:38:15 +0100

    Emacs configuration to stop tabs from reappearing.

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (indent-tabs-mode)
+  (fill-column . 80)
+  (show-trailing-whitespace . t)
+  (indent-tabs-mode)))
+
+
